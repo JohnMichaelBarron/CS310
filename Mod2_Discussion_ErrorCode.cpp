5d7776226bae3a58d951354980fa8d2288434733
John-Michael Barron <johbarron@my365.bellevue.edu>
2025-03-20 13:51:11 -0500

    Add files via upload
    Create a simple C++ program using input/output processes and at least two errors.  The program should not execute and there should be at least two errors in the code.

---
+++ Mod2_Discussion_ErrorCode.cpp
@@ -0,0 +1,15 @@
+#include <iostream> 
+using namespace std;
+
+int man() { 
+    int num1, num2;
+
+    cout << "Enter two numbers: " << endl;
+    cin >> num1 >> num2;
+
+    cout << "The sum is: " << num1 + num2 << endl
+    
+    cout << "The product is: " << result << endl;
+
+    return 0;
+}
